9722484c0c66283191d7c3d2d937b484b8642c8d
Abdullahkashif47 <125899121+Abdullahkashif47@users.noreply.github.com>
2023-02-20 20:24:16 +0500

    Create practicecode_1.cpp

---
+++ practicecode_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){#include <iostream>
+using namespace std;
+
+int main() {
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    cout<<"Fibinacco series";
+    
+    
